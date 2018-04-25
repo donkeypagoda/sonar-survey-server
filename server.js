60561a7a6bae4a9eb06d537ee348c8087d89d44f
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-25 13:42:23 -0700

    building simple server

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const cors = require ("cors")
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log("sonar survey server is listening on ", port)
+})
