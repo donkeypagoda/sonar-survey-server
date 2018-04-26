6224cbd86e781c52c247e8f16b142cb5252f2f6c
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 14:20:45 -0700

    migration made for users

--- migrations/20180426141438_users.js
+++ migrations/20180426141438_users.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("users", table => {
+    table.increments();
+    table.string("name").notNullable().unique();
+    table.string("email").notNullable().unique();
+    table.string("hashed_password").notNullable();
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("users")
 };
