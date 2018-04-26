60c036b4db625d2664d51a2482b5484df2e50bad
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 15:59:09 -0700

    made a answers seed

--- seeds/04_answers.js
+++ seeds/04_answers.js
@@ -1,13 +1,45 @@
+const answers = [
+  {
+    "question_id": 1,
+    "answer_array": null
+  }, {
+    "question_id": 2,
+    "answer_array": ["chicken", "beef", "beans", "pork"]
+  }, {
+    "question_id": 3,
+    "answer_array": ["salsa", "cheese", "sour cream", "guacamole"]
+  }, {
+    "question_id": 4,
+    "answer_array": null
+  }, {
+    "question_id": 5,
+    "answer_array": null
+  }, {
+    "question_id": 6,
+    "answer_array": null
+  }, {
+    "question_id": 7,
+    "answer_array": ["Republican", "Democrat", "Nazi", "Commie"]
+  }, {
+    "question_id": 8,
+    "answer_array": null
+  }
+]
+
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+  return knex('answers').del()
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('answers_id_seq', 1, false);"
+      )
+    })
+    .then(() => {
+      return knex('answers').insert(answers);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('answers_id_seq', (SELECT MAX(id) FROM answers));"
+      );
     });
 };
