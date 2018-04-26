b02c205883883041230492e1afc1969e5bd4128f
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 16:24:53 -0700

    simple token route added to server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require ("express")
 const bodyParser = require ("body-parser")
 const users = require ("./routes/users")
+const token = require("./routes/token")
 const cors = require ("cors")
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+app.use(users)
+app.use(token)
 
 const port = process.env.PORT || 5000
 
