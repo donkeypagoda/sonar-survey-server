6d05a3299a661f1541c74079dfb9d7b5aeabcf54
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 16:19:30 -0700

    server is working with the basic route

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,11 @@ router.post('/users', (req, res, next) => {
   knex('users').where('email', req.body.email).first()
     .then((row) => {
       if (row) {
-        return next(console.log('Email is already registered to a user.')));
+        return next(console.log('Email is already registered to a user.'));
       }
       bcrypt.hash(req.body.password, 12)
         .then((hashed_pass) => {
-          const newUser = {'name': req.body.username, 'email': req.body.email,'hashed_password': hashed_pass,};
+          const newUser = {'name': req.body.username, 'email': req.body.email,'hashed_password': hashed_pass};
           return knex('users').insert(newUser, '*')
         })
         .then((result) => {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require ("express")
 const bodyParser = require ("body-parser")
+const users = require ("./routes/users")
 const cors = require ("cors")
 
 const app = express()
