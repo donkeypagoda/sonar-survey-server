4630707e6a17198ba75c6ca73f5037892ae71cdb
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 14:27:14 -0700

    made a survey migration

--- migrations/20180426141451_surveys.js
+++ migrations/20180426141451_surveys.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("surveys", table =>{
+    table.increments();
+    table.string("title").notNullable().unique();
+    table.string("url").notNullable().unique();
+    table.integer("user_id").references("user_id") //.notNullable().onDelete('CASCADE'); - I think I want to be able to delete a user without deleting their survey
+    table.timestamps(true, true);
+
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("surveys")
+
 };
