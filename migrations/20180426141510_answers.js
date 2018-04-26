cf6b7e0521fca5f4e75322f2f00929b50b735606
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 14:33:56 -0700

    made an answers migration

--- migrations/20180426141510_answers.js
+++ migrations/20180426141510_answers.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("answers", table => {
+    table.increments();
+    table.integer("question_id").references("question_id").notNullable().onDelete('CASCADE');
+    table.json("answer_array").defaultTo([])
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("answers")
 };
