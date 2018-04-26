6a654b69fa497bbe8673fe2d830ef25b9b0a4880
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 15:35:33 -0700

    made a seed for the users table

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -1,13 +1,30 @@
+//hashed passwords were created using a WF of 12
+
+const users = [
+  {
+    'name': 'puppy',
+    'email': 'puppy@gmail.com',
+    'hashed_password': '$2a$12$xJEHUFKQ0W2hKu5pPmvttetkIpIheqQ0se3juBrN0EaiejgJ3Qmq2'
+  }, {
+    'name': 'kitty',
+    'email': 'kitty@gmail.com',
+    'hashed_password': '$2a$12$QOvBdL3pbqxgmOmk9v1nuuI61WgrWIgpRDohe96y9JWeWjRSigHPe'
+  }
+];
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+  return knex('users').del()
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('users_id_seq', 1, false);"
+      )
+    })
+    .then(() => {
+      return knex('users').insert(users);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));"
+      );
     });
 };
