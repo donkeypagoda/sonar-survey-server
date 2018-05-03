0ef71d8e658d396c82b6f4ceeaca74b696996b4c
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-03 16:10:56 -0700

    building routes, might have to re-think the db structure

--- routes/results.js
+++ routes/results.js
@@ -1,4 +1,21 @@
 const expres = require("express")
 const knex = require("../knex")
 
+router.get("/results/:id" (req, res, next) => {
+  knex("responses").where("survey_id", req.params.survey_id)
+    .then((results) => {
+      res.send(results)
+    })
+    .catch(err => next(err))
+})
+
+router.post("/results/:id" (req, res, next) => {
+  newResponse = {
+    "survey_id": req.body.survey_id;
+    "question_id": req.body.question_id  ///hmmmm this needs to be either an iterative insert, OR MORE LIKELY I NEED TO CHANGE MY DATABASE STRUCTURE!!
+
+  }
+  knex("responses").where("sur")
+})
+
 module.exports = router
