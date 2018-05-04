b5c8359c9bde2e961221bd7c294b9920630abde0
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-04 13:18:26 -0700

    questions route

--- routes/questions.js
+++ routes/questions.js
@@ -3,5 +3,18 @@ const knex = require("../knex")
 
 const router = express.Router()
 
+router.get('/questions/:id', (req, res, next) =>{
+  knex("questions").where("id", req.params.id)
+    .then(question => {
+      res.send(question)
+    })
+    .catch(err => next(err))
+})
+
+router.get("questions/:survey_id", req, res, next) => {
+  knex('questions').where("survey_id", req.params.id)
+    .then(questions => res.send(questions))
+    .catch(err => next(err))
+}
 
 module.exports = router
