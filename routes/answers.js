8f83b74a4eea25ba8f251c539fb5121062c94db0
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-04 13:49:07 -0700

    building answers route

---
+++ routes/answers.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const knex = require("../knex")
+
+const router = express.Router()
+
+router.get("/answers/:id", (req, res, next) => {
+  knex('answers').where("question_id", req.params.id)
+    .then(answers => res.send(answers))
+    .catch((err) => next(err))
+})
+
+router.post("/answers", (req, res, next) => {
+  knex("answers").insert({
+                          "question_id": req.body.question_id,
+                          "answer_array": re.body.answer_array
+                        }, '*')
+})
+
+module.exports = router;
