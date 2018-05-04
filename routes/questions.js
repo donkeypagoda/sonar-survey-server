40596aae51d509868bfbf9a95154ec72b602a828
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-04 13:27:04 -0700

    added post to questions

--- routes/questions.js
+++ routes/questions.js
@@ -9,9 +9,23 @@ router.get('/questions/:id', (req, res, next) =>{
     .catch(err => next(err))
 })
 
-router.get("questions/:survey_id", (req, res, next) => {
+router.get("/questions/:survey_id", (req, res, next) => {
   knex('questions').where("survey_id", req.params.id)
     .then(questions => res.send(questions))
+    .catch(err => next(err))
+})
+
+router.post("/questions", (req, res, next) => {
+  const newQuestion = {
+    "survey_id": req.body.survey_id,
+    "prompt": req.body.prompt,
+    "answer_type": req.body.prompt
+  }
+  return knex("questions").insert(newQuestion. "*")
+    .then((result) => {
+      res.send(result)
+      console.log(result)
+    })
     .catch(err => next(err))
 })
 
