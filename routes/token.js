4e4afa6c3e4202126b34ffc1844b0e8615703b8a
dude <christopherallenstewart@gmail.com>
2018-05-15 12:22:19 -0700

    switching over to bcryptjs from bcrypt-as-promised

--- routes/token.js
+++ routes/token.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bcrypt = require('bcrypt-as-promised')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const knex = require('../knex')
 
