175ac37eeb4fba133678da3705dbf8d2536e3802
Christopher Stewart <christopher.stewart@slalom.com>
2018-07-09 16:37:23 -0700

    learning how to work on windows

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/sonar-survey'
+    connection: 'postgres:\\sonar-survey'
   },
   production: {
     client: 'pg',
